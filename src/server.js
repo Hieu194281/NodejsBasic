f53e3619960a9b27bb65ccdeedb496f266bbab8b
NguyenTrungHieu <trunghieu220801@gmail.com>
2023-04-24 14:14:49 +0700

    router & mvc

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 import configViewEngine from "./config/viewEngine";
+import initWebRoute from "./route/web.js";
 require("dotenv").config();
 
 // import thư viện express
 const app = express(); // tạo app để chạy được
-const port = process.env.PORT || 8080; // tạo cổng
-
+const port = process.env.PORT; // tạo cổng
+//set up view engine
 configViewEngine(app);
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-app.get("/about", (req, res) => {
-  res.send("I'm Hiếu ");
-});
+
+//set up route
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
